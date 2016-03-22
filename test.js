66fbcee4abeba454086fbc1edfa8e85ca22e9e52
takayasu <a.takayasu@gmail.com>
2016-03-22 08:58:32 +0000

    NoteJS Start!

---
+++ test.js
@@ -0,0 +1,11 @@
+
+var sorted = require('user/sortutil');
+var assert = require('chai').assert;
+
+describe('挿入ソート',function(){
+ it('テスト一つ目',function(){
+     var testData = [10,3,6,1,7,9];
+    　assert.deepEqual([1,3,6,7,9,10],sorted.insertSort(testData));
+ })
+    
+})
